3a02d5330750a8a3a9f94b74c1d34bab2f5e0ca9
Alexandre Costantini <alexandre.costantini@exoalpha.com>
2026-04-05 10:57:04 +0200

    chore: fix performance issues

--- tsv.go
+++ tsv.go
@@ -12,9 +12,9 @@ const (
 	eol = 0x0A
 )
 
-type encoderFunc func(v reflect.Value) ([]byte, error)
+type encoderFunc func(buf *bytes.Buffer, v reflect.Value) error
 
-// Encoder defines struture for TSV Encoder.
+// Encoder defines structure for TSV Encoder.
 type Encoder struct {
 	nice       bool
 	timeFormat string
@@ -35,11 +35,9 @@ func (e *Encoder) Encode(v any) ([]byte, error) {
 	val := reflect.ValueOf(v)
 	encoder := e.typeEncoder(val.Type())
 
-	b, err := encoder(val)
-	if err != nil {
+	if err := encoder(&buf, val); err != nil {
 		return buf.Bytes(), err
 	}
-	buf.Write(b)
 
 	return buf.Bytes(), nil
 }
@@ -59,166 +57,168 @@ func (e *Encoder) typeEncoder(typ reflect.Type) encoderFunc {
 	case reflect.String:
 		return stringEncoder
 	case reflect.Interface:
-		return e.interfaceEncoder
+		return e.interfaceEncoderFn()
 	case reflect.Struct:
 		name := fmt.Sprintf("%s.%s", typ.PkgPath(), typ.Name())
 		if name == "time.Time" {
 			if e.nice {
-				return e.timeNiceEncoder
+				return e.timeNiceEncoderFn()
 			}
 			return timeEncoder
 		}
-		return e.structEncoder
+		return e.structEncoderFn(typ)
 	case reflect.Map:
 		return unsupportedTypeEncoder
 	case reflect.Slice:
-		return e.sliceEncoder
+		return e.sliceEncoderFn(typ)
 	case reflect.Array:
-		return e.arrayEncoder
+		return e.arrayEncoderFn(typ)
 	case reflect.Ptr:
-		return e.interfaceEncoder
+		return e.interfaceEncoderFn()
 	default:
 		return unsupportedTypeEncoder
 	}
 }
 
-func (e *Encoder) arrayEncoder(val reflect.Value) ([]byte, error) {
-	var buf bytes.Buffer
+func (e *Encoder) arrayEncoderFn(typ reflect.Type) encoderFunc {
+	elemType := typ.Elem()
+	encoder := e.typeEncoder(elemType)
 
-	t := val.Type().Elem()
-	encoder := e.typeEncoder(t)
-	for i := 0; i < val.Len(); i++ {
-		if i > 0 {
-			k := t.Kind()
-			if k == reflect.Array ||
-				k == reflect.Slice ||
-				k == reflect.Struct {
-				buf.WriteByte(eol)
-			} else {
-				buf.WriteByte(tab)
+	return func(buf *bytes.Buffer, val reflect.Value) error {
+		for i := range val.Len() {
+			if i > 0 {
+				k := elemType.Kind()
+				if k == reflect.Array ||
+					k == reflect.Slice ||
+					k == reflect.Struct {
+					buf.WriteByte(eol)
+				} else {
+					buf.WriteByte(tab)
+				}
+			}
+			if err := encoder(buf, val.Index(i)); err != nil {
+				return err
 			}
 		}
-		b, err := encoder(val.Index(i))
-		if err != nil {
-			return buf.Bytes(), err
-		}
-		buf.Write(b)
+		return nil
 	}
-	return buf.Bytes(), nil
 }
 
-func (e *Encoder) interfaceEncoder(val reflect.Value) ([]byte, error) {
-	var buf bytes.Buffer
-	if val.IsNil() {
-		buf.WriteString("null")
-	} else {
-		val := val.Elem()
-		encoder := e.typeEncoder(val.Type())
-		b, err := encoder(val)
-		if err != nil {
-			return buf.Bytes(), err
+func (e *Encoder) interfaceEncoderFn() encoderFunc {
+	return func(buf *bytes.Buffer, val reflect.Value) error {
+		if val.IsNil() {
+			buf.WriteString("null")
+		} else {
+			val := val.Elem()
+			encoder := e.typeEncoder(val.Type())
+			return encoder(buf, val)
 		}
-		buf.Write(b)
+		return nil
 	}
-	return buf.Bytes(), nil
 }
 
-func (e *Encoder) sliceEncoder(val reflect.Value) ([]byte, error) {
-	var buf bytes.Buffer
-	if val.IsNil() {
-		buf.WriteString("null")
-	} else {
-		b, err := e.arrayEncoder(val)
-		if err != nil {
-			return buf.Bytes(), err
+func (e *Encoder) sliceEncoderFn(typ reflect.Type) encoderFunc {
+	enc := e.arrayEncoderFn(typ)
+
+	return func(buf *bytes.Buffer, val reflect.Value) error {
+		if val.IsNil() {
+			buf.WriteString("null")
+			return nil
 		}
-		buf.Write(b)
+		return enc(buf, val)
 	}
-	return buf.Bytes(), nil
 }
 
-func (e *Encoder) structEncoder(val reflect.Value) ([]byte, error) {
-	var buf bytes.Buffer
-
-	for i := 0; i < val.NumField(); i++ {
-		if i > 0 {
-			buf.WriteByte(tab)
-		}
-		f := val.Field(i)
-		encoder := e.typeEncoder(f.Type())
-		b, err := encoder(f)
-		if err != nil {
-			return buf.Bytes(), err
-		}
-		buf.Write(b)
+func (e *Encoder) structEncoderFn(typ reflect.Type) encoderFunc {
+	n := typ.NumField()
+	type fieldInfo struct {
+		encoder encoderFunc
+		index   int
 	}
-	return buf.Bytes(), nil
+	fields := make([]fieldInfo, n)
+	for i := range n {
+		fields[i] = fieldInfo{
+			encoder: e.typeEncoder(typ.Field(i).Type),
+			index:   i,
+		}
+	}
+
+	return func(buf *bytes.Buffer, val reflect.Value) error {
+		for i, fi := range fields {
+			if i > 0 {
+				buf.WriteByte(tab)
+			}
+			if err := fi.encoder(buf, val.Field(fi.index)); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
 }
 
-func boolEncoder(val reflect.Value) ([]byte, error) {
-	var buf bytes.Buffer
-
+func boolEncoder(buf *bytes.Buffer, val reflect.Value) error {
 	if val.Bool() {
 		buf.WriteString("true")
 	} else {
 		buf.WriteString("false")
 	}
-	return buf.Bytes(), nil
+	return nil
 }
 
-func intEncoder(val reflect.Value) ([]byte, error) {
-	b := []byte{}
-	return strconv.AppendInt(b, val.Int(), 10), nil
+func intEncoder(buf *bytes.Buffer, val reflect.Value) error {
+	buf.Write(strconv.AppendInt(nil, val.Int(), 10))
+	return nil
 }
 
-func uintEncoder(val reflect.Value) ([]byte, error) {
-	b := []byte{}
-	return strconv.AppendUint(b, val.Uint(), 10), nil
+func uintEncoder(buf *bytes.Buffer, val reflect.Value) error {
+	buf.Write(strconv.AppendUint(nil, val.Uint(), 10))
+	return nil
 }
 
-func float32Encoder(val reflect.Value) ([]byte, error) {
-	b := []byte{}
-	return strconv.AppendFloat(b, val.Float(), 'f', -1, 32), nil
+func float32Encoder(buf *bytes.Buffer, val reflect.Value) error {
+	buf.Write(strconv.AppendFloat(nil, val.Float(), 'f', -1, 32))
+	return nil
 }
 
-func float64Encoder(val reflect.Value) ([]byte, error) {
-	b := []byte{}
-	return strconv.AppendFloat(b, val.Float(), 'f', -1, 64), nil
+func float64Encoder(buf *bytes.Buffer, val reflect.Value) error {
+	buf.Write(strconv.AppendFloat(nil, val.Float(), 'f', -1, 64))
+	return nil
 }
 
-func stringEncoder(val reflect.Value) ([]byte, error) {
-	var buf bytes.Buffer
+func stringEncoder(buf *bytes.Buffer, val reflect.Value) error {
 	buf.WriteString(val.String())
-	return buf.Bytes(), nil
+	return nil
 }
 
-func timeEncoder(val reflect.Value) ([]byte, error) {
+func timeEncoder(buf *bytes.Buffer, val reflect.Value) error {
 	_, ok := val.Type().MethodByName("Unix")
 	if !ok {
-		return nil, fmt.Errorf("wrong time value: %s", val.Type().String())
+		return fmt.Errorf("wrong time value: %s", val.Type().String())
 	}
 	method := val.MethodByName("Unix")
 	res := method.Call([]reflect.Value{})
 	if len(res) != 1 {
-		return nil, fmt.Errorf("wrong time value: %s", val.Type().String())
+		return fmt.Errorf("wrong time value: %s", val.Type().String())
 	}
-	return intEncoder(res[0])
+	return intEncoder(buf, res[0])
 }
 
-func (e *Encoder) timeNiceEncoder(val reflect.Value) ([]byte, error) {
-	_, ok := val.Type().MethodByName("Format")
-	if !ok {
-		return nil, fmt.Errorf("wrong time value: %s", val.Type().String())
+func (e *Encoder) timeNiceEncoderFn() encoderFunc {
+	return func(buf *bytes.Buffer, val reflect.Value) error {
+		_, ok := val.Type().MethodByName("Format")
+		if !ok {
+			return fmt.Errorf("wrong time value: %s", val.Type().String())
+		}
+		method := val.MethodByName("Format")
+		in := []reflect.Value{reflect.ValueOf(e.timeFormat)}
+		res := method.Call(in)
+		if len(res) != 1 {
+			return fmt.Errorf("wrong time value: %s", val.Type().String())
+		}
+		return stringEncoder(buf, res[0])
 	}
-	method := val.MethodByName("Format")
-	in := []reflect.Value{reflect.ValueOf(e.timeFormat)}
-	res := method.Call(in)
-	if len(res) != 1 {
-		return nil, fmt.Errorf("wrong time value: %s", val.Type().String())
-	}
-	return stringEncoder(res[0])
 }
 
-func unsupportedTypeEncoder(val reflect.Value) ([]byte, error) {
-	return nil, fmt.Errorf("unsupported type: %s", val.Type().String())
+func unsupportedTypeEncoder(buf *bytes.Buffer, val reflect.Value) error {
+	return fmt.Errorf("unsupported type: %s", val.Type().String())
 }
